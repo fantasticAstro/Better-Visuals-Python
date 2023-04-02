8ad3c0d3f48fbc76677cea7e8e1b09f924ccb482
Kavish <khukmani@gmail.com>
2023-04-02 15:09:53 -0700

    Remove unnecessary dashboards

--- utils/db_util.py
+++ utils/db_util.py
@@ -15,29 +15,3 @@ def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
-
-class Dashboard1Table1(db.Model):
-    __tablename__ = [storage['name'] for storage in dashboard_storage_metadata['dashboard1'] if storage['type'] == 'SQL'][0]
-    email = db.Column(db.String(120), primary_key=True)
-    value = db.Column(db.String(120), nullable=False)
-
-    def __init__(self, email, value):
-        self.email = email
-        self.value = value
-
-    def __repr__(self):
-        return f'<UserInput {self.email} {self.value}>'
-
-
-class SpotifyClientID(db.Model):
-    __tablename__ = [storage['name'] for storage in dashboard_storage_metadata['spotify_test'] if storage['type'] == 'SQL'][0]
-    email = db.Column(db.String(120), primary_key=True)
-    clientid = db.Column(db.String(120), nullable=False)
-
-    def __init__(self, email, clientid):
-        self.email = email
-        self.clientid = clientid
-
-    def __repr__(self):
-        return f'<UserInput {self.email} {self.clientid}>'
