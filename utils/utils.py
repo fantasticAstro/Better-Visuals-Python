6571ef1de75166094406945a3f93d8a9e332a11a
Kavish <khukmani@gmail.com>
2023-04-02 12:52:27 -0700

    Add utils.py

---
+++ utils/utils.py
@@ -0,0 +1,25 @@
+import os
+
+
+def save_data_to_disk(data, filename, save_folder, userid):
+    DATAPATH_FOLDER = os.path.join('saved_data', save_folder)
+    DATAPATH_USER = os.path.join(DATAPATH_FOLDER, userid)
+
+    if not os.path.exists(DATAPATH_FOLDER):
+        os.makedirs(DATAPATH_FOLDER)
+
+    if not os.path.exists(DATAPATH_USER):
+        os.makedirs(DATAPATH_USER)
+
+    with open(os.path.join(DATAPATH_USER, filename), 'w') as f:
+        f.write(data)
+
+
+def fetch_data_from_disk(filename, save_folder, userid):
+    with open(os.path.join('saved_data', save_folder, userid, filename), 'r') as f:
+        data = f.read()
+    return data
+
+
+def check_if_saved_data_exists(filename_list, save_folder, userid):
+    return all(os.path.exists(os.path.join('saved_data', save_folder, userid, filename)) for filename in filename_list)
